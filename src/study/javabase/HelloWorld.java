9d9bb55c8044007dc1abd8ea202bcc4048903279
wangfei925747897 <61128876+wangfei925747897@users.noreply.github.com>
2020-05-17 14:47:56 +0800

    init

---
+++ src/study/javabase/HelloWorld.java
@@ -0,0 +1,20 @@
+package study.javabase;
+
+public class HelloWorld extends Runnable{
+    int a;
+
+    public int gaetA() {
+        return a;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+
+}
